af892253d68a07e5165ad559ffd47b8774ff510c
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-06-20 11:50:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@
 # In[ ]:
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
+app.config['DATABASE_URI'] = 'postgres://aybqvbmvydwbkw:17c7d6273effa4918c9c8a9423422f227fb111b7b12327d299222ac88e9dce60@ec2-54-83-59-120.compute-1.amazonaws.com:5432/d7abullalhhj2j'
 
