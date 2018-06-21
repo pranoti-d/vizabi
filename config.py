32fdf78aa67eff0b1bff8b1c1c01639bc68a6cd3
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-06-21 08:27:27 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://aybqvbmvydwbkw:17c7d6273effa4918c9c8a9423422f227fb111b7b12327d299222ac88e9dce60@ec2-54-83-59-120.compute-1.amazonaws.com:5432/d7abullalhhj2j'
