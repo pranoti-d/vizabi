a314dafd9f48c1d197e210453390e144cf8b268b
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-06-23 09:05:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql://bd41fb7755cb95:e89a06f5@us-cdbr-iron-east-04.cleardb.net/heroku_d45ae70ddfab245?reconnect=true'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
