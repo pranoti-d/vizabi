87d5f3de8a67abb6d8bd8ffdb70da554df6b077b
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-06-23 10:13:54 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,6 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import pymysql
-
-pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 app.config.from_object(Config)
