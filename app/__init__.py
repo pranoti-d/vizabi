aeeea0eff89350c4779616fbadfbfee60e3a85c3
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-06-26 08:37:05 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = MySQL(app)
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
