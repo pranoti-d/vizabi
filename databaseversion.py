aa708b636d0f7b034fe41a0c312db267a54dbde9
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-06-27 11:58:20 +0530

    Update databaseversion.py

--- databaseversion.py
+++ databaseversion.py
@@ -1,1 +1,21 @@
+from app import server
 from app import app
+
+app.layout = html.Div(children=[
+    html.Div(children='''
+    Dash: A web application framework for Python
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 6], 'type': 'bar', 'name': 'Montreal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
