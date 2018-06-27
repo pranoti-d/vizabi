11e05db6d12962d70c8a216bd7c80fd97f9ecda9
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-06-27 11:05:47 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL
 from flask_migrate import Migrate
 
-app = Flask(__name__)
+#app = Flask(__name__)
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
