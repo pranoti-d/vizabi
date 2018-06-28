bda01ab96c4711af02571e9021d5e6f58bc3bcec
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-06-28 09:26:56 +0530

    Update databaseversion.py

--- databaseversion.py
+++ databaseversion.py
@@ -1,24 +1,170 @@
 from app import server
 from app import app
 
-app.layout = html.Div(children=[
-    html.Div(children='''
-    Dash: A web application framework for Python
-    '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 6], 'type': 'bar', 'name': 'Montreal'},
+
+# In[2]:
+
+
+file = pd.read_csv( "Test Data_Dummy Data.csv", encoding = "ISO-8859-1")
+file.iloc[:,2:20] = file.iloc[:,2:20].apply(lambda x : round(x, 2))
+
+
+# In[ ]:
+
+
+def select_chart(x_axis,y_axis,chart_type) :
+    data_chart = file
+    dataPanda = create_trace(data_chart,x_axis,y_axis,chart_type)
+    return dataPanda
+     
+    
+def create_trace(data_chart,x_axis,y_axis,chart_type) :    
+    dataPanda = []
+    for i in data_chart.iloc[:,2:20].columns.unique():
+        trace = go.Scatter(
+                    x=data_chart[data_chart['Metric'] == x_axis][i],
+                    y=data_chart[data_chart['Metric'] == y_axis][i],
+                    text= i,
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i  ) 
+        dataPanda.append(trace)
+    return dataPanda
+    
+def create_layout(x_axis,y_axis) : 
+    layout =  go.Layout(
+               # autosize=False,
+               # width=1000,
+                height=700,
+                xaxis = dict(type = 'log', title= x_axis),
+                yaxis=dict(title=y_axis),
+                legend=dict(orientation="h"),
+                hovermode='closest'
+            )
+    return layout
+
+
+# In[ ]:
+
+
+#app = dash.Dash(__name__, static_folder='assets')
+# app.scripts.config.serve_locally=True
+# app.css.config.serve_locally=True
+# app.config.supress_callback_exceptions=True
+
+
+app = dash.Dash(__name__, static_folder='assets')
+
+app.css.append_css({'external_url':
+#                 'https://cdn.rawgit.com/gschivley/8040fc3c7e11d2a4e7f0589ffc829a02/raw/fe763af6be3fc79eca341b04cd641124de6f6f0d/dash.css'
+                  'https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css'
+               })
+app.title = 'States of India'
+server = app.server
+
+app.layout = html.Div([
+    # title row
+    html.Div(
+        [
+            html.H1(
+                'States of India',
+                style={'font-family': 'Helvetica',
+                       "margin-top": "25",
+                       "margin-bottom": "0"},
+                className='eight columns',
+            ),
+             html.P(
+                'Explore states of India',
+                style={'font-family': 'Helvetica',
+                       "font-size": "120%",
+                       "width": "80%"},
+                className='eight columns',
+            ),
+        ], 
+         className='row'
+        ),
+    
+    # selectors
+   html.Div(
+        [ 
+     html.Div(
+        [
+            html.P('Choose x-axis:'), 
+            dcc.Dropdown(
+                    id='x_axis',
+                    options=[{'label': 'Real GDP Growth', 'value': 'Real GDP Growth'},
+                             {'label': 'Per capita GDP', 'value': 'Per capita GDP'},
+                             {'label': 'Population', 'value': 'Population'}],              
+                    value='Per capita GDP'
+                        )
             ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
+           className='four columns',
+           style={'margin-top': '10'}
+     ),
+    
+    html.Div(
+        [
+            html.P('Choose y-axis:'), 
+            dcc.Dropdown(
+                    id='y_axis',
+                    options=[{'label': 'Real GDP Growth', 'value': 'Real GDP Growth'},
+                             {'label': 'Per capita GDP', 'value': 'Per capita GDP'},
+                             {'label': 'Population', 'value': 'Population'}],              
+                    value='Population'
+                        )
+            ],
+           className='four columns',
+           style={'margin-top': '10'}
+     ),
+    
+       html.Div(
+        [
+            html.P('Choose chart-type:'), 
+            dcc.Dropdown(
+                    id='chart_type',
+                    options=[{'label': 'Scatter', 'value': 'scatter'}],              
+                    value='scatter'
+                        )
+            ],
+           className='four columns',
+           style={'margin-top': '10'}
+     )
+        ], className='row'
+   ),
+    
+  #chart
+     html.Div(
+        [
+            dcc.Graph(id='example-graph',
+                              animate=True, 
+                              style={'margin-top': '20'},
+                               config={'displayModeBar': False}
+                     )
+        ], className='row'
+         ),
+    
+    dcc.Markdown('Created by [Ashutosh Datar](https://twitter.com/adatar)')
+   
+], className='ten columns offset-by-one')    
+
+@app.callback(
+    dash.dependencies.Output('example-graph', 'figure'),
+    [dash.dependencies.Input('x_axis', 'value'),
+    dash.dependencies.Input('y_axis', 'value'),
+    dash.dependencies.Input('chart_type', 'value'),
+    ])
+def update_output(x_axis, y_axis, chart_type):
+    dataPanda = select_chart(x_axis,y_axis,chart_type)
+    layout = create_layout(x_axis,y_axis)
+    figure = {'data': dataPanda,
+            'layout': layout}
+    return figure  
+    
 
 if __name__ == '__main__':
     app.run_server(debug=True)
