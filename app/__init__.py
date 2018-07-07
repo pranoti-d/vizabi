bf26c67ec8141d47fc4c25fecc2b6e12b9c76959
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-07 22:15:09 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from config import Config
+#from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL
 from flask_migrate import Migrate
