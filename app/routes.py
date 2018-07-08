a072da22c5f2a3306fa99d3df5e9ade8bbd5a474
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-08 09:28:08 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
+from flask import render_template
 #from app import server
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Home', user=user)
