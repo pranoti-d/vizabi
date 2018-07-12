ba878eb7270cf7ab2ccdbee3e423f686c845eb10
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-12 10:25:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
   ELASTICSEARCH_URL= 'http://localhost:9200'
+    MYSQL_HOST = 'us-cdbr-iron-east-04.cleardb.net'
+    MYSQL_USER = 'bd41fb7755cb95'
+    MYSQL_PASSWORD = 'e89a06f5'
+    MYSQL_DB = 'heroku_d45ae70ddfab245'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bd41fb7755cb95:e89a06f5@us-cdbr-iron-east-04.cleardb.net/heroku_d45ae70ddfab245'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
   
