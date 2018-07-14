0dd64e9ac1566b6856216b2e9d3703fad06ac8ad
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-14 20:31:36 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -15,12 +15,5 @@ def index():
 
 @app.route('/search')
 def search():
-    page = request.args.get('page', 1, type=int)
-    posts, total = Post.search(g.search_form.q.data, page,
-                               current_app.config['POSTS_PER_PAGE'])
-    next_url = url_for('search', q=g.search_form.q.data, page=page + 1) \
-        if total > page * current_app.config['POSTS_PER_PAGE'] else None
-    prev_url = url_for('search', q=g.search_form.q.data, page=page - 1) \
-        if page > 1 else None
-    return render_template('search.html', title=_('Search'), posts=posts,
-                           next_url=next_url, prev_url=prev_url)
+    list = test_data_dummy_data.query.whoosh_search(request.args.get('query')).all
+    return render_template('search.html', title=_('Search'), posts=list)
