d35a5cb9626a38c1f362d2ceb7bff276b1d712e7
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-14 16:34:42 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Length
 from flask_babel import _, lazy_gettext as _l
-from app.models import User
+from app.models import Test_Data_Dummy_Data
 
 class PostForm(FlaskForm):
     post = TextAreaField(_l('Say something'), validators=[DataRequired()])
