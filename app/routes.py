0e166e0af4e53548bd436219c4f877879f293a0e
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-14 16:15:44 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
 from app import app
 from flask import render_template
 #from app import server
+from flask import g
+from app.forms import SearchForm
+
+@bp.before_app_request
+def before_request():
+    g.search_form = SearchForm()
+    g.locale = str(get_locale())
 
 @app.route('/')
 @app.route('/index')
