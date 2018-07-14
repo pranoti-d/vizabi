bb03cd9c710e422caa09726bc853627d3d702513
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-14 21:20:52 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import app, db
 from app.search import add_to_index, remove_from_index, query_index
 import flask_whooshalchemy as wa 
 
-wa.whoosh_index(app, test_data_dummy_data)
+#wa.whoosh_index(app, test_data_dummy_data)
 
 class SearchableMixin(object):
     @classmethod
