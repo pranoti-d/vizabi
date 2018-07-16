394ab67921540d75baeac12095637ffe46ac9109
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-16 10:31:09 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,7 @@ from wtforms import StringField, SubmitField
 class SearchForm(FlaskForm):
     seachString = StringField('search')
     submit = SubmitField('submit')
+
+class ResultForm(FlaskForm):
+    textvalue = StringField('Hello World)
+    
