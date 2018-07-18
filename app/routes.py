2cd21d30b26ef9b5c448e312a350621e42e24e3a
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-18 09:57:06 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect, url_for, request, g, \
-    jsonify, current_app
+from flask import render_template, flash, redirect, url_for, request, g, current_app
 #from app import server
 from flask import g
 from flask_babel import _, get_locale
