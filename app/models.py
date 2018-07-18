2c69b7600d53be528861a224d3d0be9c66e51942
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-18 08:37:26 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from app import app, db
 #from app.search import add_to_index, remove_from_index, query_index
-#import flask_whooshalchemy as wa 
+import flask_whooshalchemy 
 
 #wa.whoosh_index(app, test_data_dummy_data)
 
