15e05b1e6fd24df31f91fcffd132d95ea647925d
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-18 09:07:23 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from flask_babel import _, lazy_gettext as _l
 
 
 class SearchForm(FlaskForm):
     seachString = StringField('search')
+    q = StringField(_l('search'))
     submit = SubmitField('submit')
+    
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
 
 class resultForm(FlaskForm):
     textvalue = StringField('Hello World')
