d14adee0bda6f6a74266c14146408b8c072ce79e
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-29 13:57:06 +0530

    Update website.py

--- website.py
+++ website.py
@@ -1,2 +1,7 @@
 from app import app
+from app.models import test_data_dummy_data
 #from app import server
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'test_data_dummy_data': test_data_dummy_data}
