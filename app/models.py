5aeb64c1c30200e78c0b20fac80b919f3664e420
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-07-29 22:53:30 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -55,8 +55,8 @@ class SearchableIndex(object):
         for obj in cls.query:
             add_to_index(cls.__tablename__, obj)
 
-db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
-db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
+db.event.listen(db.session, 'before_commit', SearchableIndex.before_commit)
+db.event.listen(db.session, 'after_commit', SearchableIndex.after_commit)
 
 
 
