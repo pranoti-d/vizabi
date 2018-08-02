73efe4a19f6f85a5b035deff210fc4ff7322e130
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-02 07:36:57 +0530

    Update website.py

--- website.py
+++ website.py
@@ -5,3 +5,7 @@ from app import server
 @server.shell_context_processor
 def make_shell_context():
     return {'db': db, 'test_data_dummy_data': test_data_dummy_data, 'search_index': search_index}
+
+
+if __name__ == '__main__':
+     app.run_server(debug=True)
