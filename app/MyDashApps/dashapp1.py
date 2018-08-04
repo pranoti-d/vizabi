89fb96674ae4944d8bdc3f993723b63f688445c0
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-04 09:10:20 +0530

    Update dashapp1.py

--- app/MyDashApps/dashapp1.py
+++ app/MyDashApps/dashapp1.py
@@ -1,4 +1,9 @@
 from app.Dashserver import DashServer
+from dash.dependencies import Input, State, Output
+import dash_html_components as html
+import dash
+import dash_core_components as dcc
+
 
 DashServer.layout = html.Div([
 html.Div('Carrefour sales Viewer')
