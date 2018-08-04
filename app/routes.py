a8841ecc372828ab2ceb982a57215caef6974cc4
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-04 20:26:53 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -62,7 +62,7 @@ def visualization(description):
 
 
 
-DashServer.layout = html.Div([	dcc.Location(id='url', refresh=False),	dcc.Input(id='description', type='string'), html.Div(id='page-content')	])
+DashServer.layout = html.Div([	dcc.Location(id='url', refresh=False),	dcc.Input(id='description', type='text'), html.Div(id='page-content')	])
     
 @DashServer.callback(Output('page-content', 'children'),[Input('url', 'pathname')], [Input('description', 'filter')])
 def display_page(pathname, filter):
