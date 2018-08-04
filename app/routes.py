ac0421fd4cb03b62c37572af35ae6be6a5a415db
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-04 09:08:42 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ from flask_babel import _, get_locale
 from app.models import test_data_dummy_data, search_index
 from app.forms import SearchForm, resultForm
 from app.search import add_to_index
-from app.MyDashApps import DashServer.layout
+from app.MyDashApps import dashapp1
 from dash.dependencies import Input, State, Output
 import dash_html_components as html
 import dash
