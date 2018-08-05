ca020d48223ce7da2ed407433a6c01f72a069e57
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-05 10:10:08 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -68,10 +68,11 @@ DashServer.layout = html.Div([	dcc.Location(id='url', refresh=False),	dcc.Input(
     
 @DashServer.callback(Output('page-content', 'children'),[Input('url', 'pathname')], [State('description', 'filter')])
 def display_page(pathname, filter):
-	if pathname is None:
-           return 'Loading...'
-        else:
-           return dashapp1.layout(filter)
+    if pathname is None:
+	return 'Loading...'
+    else:
+	return dashapp1.layout(filter)	
+	
 	   	
         
      
