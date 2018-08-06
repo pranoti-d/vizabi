1fb9a6aea63c269ce59370366318653f0995999d
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-06 12:05:26 +0530

    Update dashapp0.py

--- app/MyDashApps/dashapp0.py
+++ app/MyDashApps/dashapp0.py
@@ -124,7 +124,7 @@ DashServer.layout = html.Div([
 layout = DashServer.layout
 
 @DashServer.callback(
-    Output('app-1-display-value', 'children'),
+    Output('page-content', 'children'),
     [Input('app-1-dropdown', 'value')])
 def display_value(value):
     return 'You have selected "{}"'.format(value)    
