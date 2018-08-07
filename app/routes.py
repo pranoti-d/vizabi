9ccd38b5c829166d46602eb9d64460d769d9686b
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-07 22:35:20 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -73,7 +73,7 @@ def display_page(pathname):
     pathname = str(pathname) 	
     if pathname.startswith('/app/'):
        des = pathname.split('/')[-1]	
-       return dashapp0.layout(description=des)
+       return dashapp1.layout(description=des)
     elif pathname == '/app/MyDashApps/dashapp1':
          return dashapp1.layout
     elif pathname == '/app/MyDashApps/dashapp0':
