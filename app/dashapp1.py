18689d318205aec5def8caa616f535e3a7149432
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-08 09:31:50 +0530

    Update dashapp1.py

--- app/dashapp1.py
+++ app/dashapp1.py
@@ -9,13 +9,13 @@ html.Div(id='graph-1')
 ])
 
 
-@DashServer.callback(Output('graph-1', 'children'), [Input('signal', 'filter')])
+@DashServer.callback(Output('graph-1', 'children'), [Input('signal', 'children')])
 def update_value_1(value):
     # generate_figure gets data from `global_store`.
     # the data in `global_store` has already been computed
     # by the `compute_value` callback and the result is stored
     # in the global redis cached
     return html.Div([
-        html.H3('You are on page {}'.format(filter)) ])
+        html.H3('You are on page {}'.format(value)) ])
 
 layout = DashServer.layout
