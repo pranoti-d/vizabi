ab80bff8e8de31fbe6ceab1c443e22993a9fdae5
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-08 08:40:18 +0530

    Update dashapp1.py

--- app/dashapp1.py
+++ app/dashapp1.py
@@ -2,5 +2,14 @@ from app.Dasshserver import DashServer
 import dash_html_components as html
 
 DashServer.layout = html.Div([
-html.Div('Carrefour sales Viewer')
+html.Div(id='graph-1')
 ])
+
+
+@DashServer.callback(Output('graph-1', 'figure'), [Input('signal', 'children')])
+def update_value_1(value):
+    # generate_figure gets data from `global_store`.
+    # the data in `global_store` has already been computed
+    # by the `compute_value` callback and the result is stored
+    # in the global redis cached
+    return return 'You have selected "{}"'.format(value)
